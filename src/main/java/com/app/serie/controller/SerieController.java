bc01c9cf51e1edcd08c507e6dc01d2772c027000
Vitor Alves <vitorchavesalv@gmail.com>
2024-05-25 18:31:28 -0300

    feat: add controller class and methods

---
+++ src/main/java/com/app/serie/controller/SerieController.java
@@ -0,0 +1,32 @@
+package com.app.serie.controller;
+
+import com.app.serie.entity.SerieDTO;
+import com.app.serie.entity.SerieDetailDTO;
+import com.app.serie.service.SerieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/serie")
+public class SerieController {
+    @Autowired
+    private SerieService serieService;
+
+    @PostMapping
+    public SerieDetailDTO save(@RequestBody SerieDTO serieDto) {
+        return this.serieService.saveSerie(serieDto);
+    }
+
+    @GetMapping
+    public List<SerieDetailDTO> get() {
+        return this.serieService.getAllSeries();
+    }
+
+    @DeleteMapping
+    public void delete(@RequestBody UUID id) {
+        this.serieService.deleteSerieById(id);
+    }
+}
